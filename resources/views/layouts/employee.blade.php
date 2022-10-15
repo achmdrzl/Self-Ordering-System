7fed6ec9ecc9378fce94a703a45d176882e47a39
achmdrzl <achmd.rzl13@gmail.com>
2022-10-15 18:49:01 +0700

    15Okt2022

--- resources/views/layouts/employee.blade.php
+++ resources/views/layouts/employee.blade.php
@@ -9,21 +9,21 @@
     <!-- plugins:css -->
     {{-- <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet"> --}}
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('backend/vendors/feather/feather.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/vendors/ti-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/vendors/css/vendor.bundle.base.css') }}">
+    <link rel="stylesheet" href="{{ asset('employee/vendors/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('employee/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('employee/vendors/css/vendor.bundle.base.css') }}">
     @stack('style-alt')
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="{{ asset('backend/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/vendors/ti-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/js/select.dataTables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('employee/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('employee/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('employee/js/select.dataTables.min.css') }}">
 
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('backend/css/vertical-layout-light/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('employee/css/vertical-layout-light/style.css') }}">
     <!-- endinject -->
-    <link rel="shortcut icon" href="{{ asset('backend/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('employee/images/favicon.png') }}" />
     @livewireStyles
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -43,9 +43,9 @@
         <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
                 <a class="navbar-brand brand-logo mr-5" href="index.html"><img
-                        src="{{ asset('backend/images/logo.svg') }}" class="mr-2" alt="logo" /></a>
+                        src="{{ asset('employee/images/logo.svg') }}" class="mr-2" alt="logo" /></a>
                 <a class="navbar-brand brand-logo-mini" href="index.html"><img
-                        src="{{ asset('backend/images/logo-mini.svg') }}" alt="logo" /></a>
+                        src="{{ asset('employee/images/logo-mini.svg') }}" alt="logo" /></a>
             </div>
             <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
                 <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -106,7 +106,7 @@
                     </li>
                     <li class="nav-item nav-profile dropdown">
                         <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-                            <img src="{{ asset('backend/images/faces/face28.jpg') }}" alt="profile" />
+                            <img src="{{ asset('employee/images/faces/face28.jpg') }}" alt="profile" />
                         </a>
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown"
                             aria-labelledby="profileDropdown">
@@ -181,13 +181,13 @@
     <!-- container-scroller -->
 
     <!-- plugins:js -->
-    <script src="{{ asset('backend/vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('employee/vendors/js/vendor.bundle.base.js') }}"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
-    <script src="{{ asset('backend/vendors/chart.js/Chart.min.js') }}"></script>
-    <script src="{{ asset('backend/vendors/datatables.net/jquery.dataTables.js') }}"></script>
-    <script src="{{ asset('backend/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
-    <script src="{{ asset('backend/js/dataTables.select.min.js') }}"></script>
+    <script src="{{ asset('employee/vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('employee/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('employee/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('employee/js/dataTables.select.min.js') }}"></script>
 
     <script>
         $(document).ready(function() {
@@ -220,15 +220,15 @@
 
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="{{ asset('backend/js/off-canvas.js') }}"></script>
-    <script src="{{ asset('backend/js/hoverable-collapse.js') }}"></script>
-    <script src="{{ asset('backend/js/template.js') }}"></script>
-    <script src="{{ asset('backend/js/settings.js') }}"></script>
-    <script src="{{ asset('backend/js/todolist.js') }}"></script>
+    <script src="{{ asset('employee/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('employee/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('employee/js/template.js') }}"></script>
+    <script src="{{ asset('employee/js/settings.js') }}"></script>
+    <script src="{{ asset('employee/js/todolist.js') }}"></script>
     <!-- endinject -->
     <!-- Custom js for this page-->
-    {{-- <script src="{{ asset('backend/js/dashboard.js') }}"></script>
-    <script src="{{ asset('backend/js/Chart.roundedBarCharts.js') }}"></script> --}}
+    {{-- <script src="{{ asset('employee/js/dashboard.js') }}"></script>
+    <script src="{{ asset('employee/js/Chart.roundedBarCharts.js') }}"></script> --}}
     <!-- End custom js for this page-->
     @stack('script-alt')
     @livewireScripts
